55fa31e31910c260e4056a2d023285291cec86a5
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-02 02:45:10 +0530

    fog: Update to V14.0.2.0.TGEMIXM
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_bengal_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp_india.xml
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.audio.policy.engine.odm=true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1650
 TARGET_SCREEN_WIDTH := 720
@@ -37,6 +40,10 @@ TARGET_SCREEN_WIDTH := 720
 # Camera
 PRODUCT_PACKAGES += \
     libpiex_shim
+
+# Display
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig)
 
 # Init script
 PRODUCT_PACKAGES += \
